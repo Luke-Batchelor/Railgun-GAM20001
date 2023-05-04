88c409dfef129d06c839672f34af08e2c4c843a2
Maddi <maddi.fitzmaurice@gmail.com>
2023-05-04 22:44:11 +1000

    DebrisManager Setup
    - Set up DebrisManager
    - Set up Debris prefab

---
+++ Assets/Scripts/DebrisManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DebrisManager : MonoBehaviour
+{
+    [SerializeField] GameObject _debrisPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
