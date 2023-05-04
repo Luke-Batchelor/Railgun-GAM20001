b6b29599548ef403adc12824112b641c3e4e5763
Maddi <maddi.fitzmaurice@gmail.com>
2023-05-04 22:53:04 +1000

    Debris Spawn Positions
    - Set up basic spawn positions for debris

--- Assets/Scripts/DebrisManager.cs
+++ Assets/Scripts/DebrisManager.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 
 public class DebrisManager : MonoBehaviour
 {
+    // Debris Data
     [SerializeField] GameObject _debrisPrefab;
+    [SerializeField] int _debrisMaxCount;
+    [SerializeField] List<Transform> _debrisSpawnList;
+
+    // Debris List
+    List<GameObject> _debrisPool;
 
     void Start()
     {
-        
+        // Create Debris pool
+        _debrisPool = ObjectPooler.CreateObjectPool(_debrisMaxCount, _debrisPrefab);
     }
 
     void Update()
