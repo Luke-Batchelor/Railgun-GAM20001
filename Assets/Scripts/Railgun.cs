5ee66f126bcae5a43910bcd12f6d2227c3ff1df3
Maddi <maddi.fitzmaurice@gmail.com>
2023-05-04 17:03:53 +1000

    Railgun Hit Detection
    - Railgun detects hits (currently just the satellite)

--- Assets/Scripts/Railgun.cs
+++ Assets/Scripts/Railgun.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Railgun : MonoBehaviour
 {
+    // Components
     private Rigidbody2D _rb;
+
+    // Inputs
     private Vector3 _lookDir;
     private float _mouseDistance;
+
+    // Target Info
+    RaycastHit2D _hit;
+    bool _isOnTarget;
 
     void Start()
     {
@@ -15,26 +22,49 @@ public class Railgun : MonoBehaviour
 
     void Update()
     {
+        RotateGun();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireGun();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        _hit = Physics2D.Raycast(transform.position, _lookDir, _mouseDistance);
+
+        if (_hit.collider != null)
+        {
+            _isOnTarget = true;
+            Debug.DrawRay(transform.position, _lookDir * _mouseDistance, Color.green);
+        }
+        else
+        {
+            _isOnTarget = false;
+            Debug.DrawRay(transform.position, _lookDir * _mouseDistance, Color.red);
+        }
+    }
+
+    void RotateGun()
+    {
         // Get mouse position
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
 
-        _mouseDistance = (mousePos - transform.position).magnitude; 
+        _mouseDistance = (mousePos - transform.position).magnitude;
 
         _lookDir = mousePos - transform.position;
         Quaternion lookRot = Quaternion.LookRotation(_lookDir, Vector3.back);
         _rb.MoveRotation(lookRot);
     }
 
-    void FixedUpdate()
+    void FireGun()
     {
-        if (Physics2D.Raycast(transform.position, _lookDir, _mouseDistance))
+        if (_isOnTarget)
         {
-            Debug.DrawRay(transform.position, _lookDir * _mouseDistance, Color.green);
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, _lookDir * _mouseDistance, Color.red);
+            Debug.Log("BOOM");
+            _isOnTarget = false;
         }
     }
 }
