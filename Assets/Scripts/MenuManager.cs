ff7756ee4d9749a840bf3e800a6f5ab5d1cc704c
Maddi <maddi.fitzmaurice@gmail.com>
2023-06-03 18:04:23 +1000

    Instructions Menu Functionality
    - can now go back and forth between instructions and main menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,8 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] GameObject _mainMenuUI;
+    [SerializeField] GameObject _instructionsUI;
+
+    private void Start()
+    {
+        _mainMenuUI.SetActive(true);
+        _instructionsUI.SetActive(false);
+    }
+
     public void Play()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ShowInstructions(bool toggle)
+    {
+        _instructionsUI.SetActive(toggle);
+        _mainMenuUI.SetActive(!toggle);
     }
 }
